a114cd3837e67940cd6eaea9039d78a55cba2aff
kotalka121212 <256168069+kotalka121212-alt@users.noreply.github.com>
2026-04-19 15:15:21 +0500

    studio1.0.0

--- src/files/includes.h
+++ src/files/includes.h
@@ -12,7 +12,6 @@
 
 //boost ->
 
-#include <boost/filesystem.hpp>     // для работы с файлами
 #include <boost/algorithm/string.hpp> // для обработки строк
 #include <boost/lexical_cast.hpp>   // для конвертации
 #include <boost/property_tree/json_parser.hpp> // если нужен второй JSON парсер
